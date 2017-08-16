12658b724a35b9a1717d33f884de7212b948ff6f
kiathan <me@kiathan.com>
2017-08-16 16:28:57 +0800

    update emailtester.py

--- emailtester.py
+++ emailtester.py
@@ -5,14 +5,14 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.Utils import formatdate
 
-if len(sys.argv) != 3:
-	print "Usage: emailtester.py [source email] [password] [attachment] [destination email]"
+if len(sys.argv) != 6:
+	print "Usage: emailtester.py [source email] [password] [SMTP server] [attachment] [destination email]"
 	exit()
 
 msg = MIMEMultipart()
 
 mfrom = sys.argv[1]
-mto = sys.argv[4]
+mto = sys.argv[5]
 mpassword = sys.argv[2]
 
 msg['Subject'] = 'Test case'
@@ -21,7 +21,7 @@ msg['To'] = mto
 msg['Date'] = formatdate()
 
 # Open the file to scan in binary mode
-fp = open(sys.argv[3], 'rb')
+fp = open(sys.argv[4], 'rb')
 attachment = MIMEBase('application', 'octet-stream')
 attachment.set_payload(fp.read())
 encoders.encode_base64(attachment)
@@ -30,7 +30,7 @@ fp.close()
 msg.attach(attachment)
 
 # Send the email via your own SMTP server.
-s = smtplib.SMTP('smtp.outlook.com', 587)
+s = smtplib.SMTP(sys.argv[3], 587)
 s.ehlo()
 s.starttls()
 s.login(mfrom, mpassword)
